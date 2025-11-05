1cb71cc1dd7c70a86e601f8057180718dc7528ab
Aleix <aleix.martinez@enti.cat>
2025-11-05 01:29:49 +0100

    BulletPrefab

---
+++ js/prefabs/bulletPrefab.js
@@ -0,0 +1,17 @@
+class bulletPrefab extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, key = 'bullet') {
+    super(scene, x, y, key);
+    scene.add.existing(this); 
+   
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+    
+    if (this.x < 0 || this.x > config.width || this.y < 0 || this.y > config.height) {
+      this.setActive(false);
+      this.setVisible(false);
+      if (this.body) this.body.reset(-100, -100);
+    }
+  }
+}
