b06feb687c23c57da633584bb94c875be2e6b448
loqatis <marc.serra@enti.cat>
2025-11-25 15:56:51 +0100

    Level start anim
    Text to be desplayed

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,3 +1,4 @@
+import { GAME_SIZE } from "../core/constants.js";
 
 class menu extends Phaser.Scene {
   constructor() { super({ key: "menuScene" }); }
@@ -7,6 +8,8 @@ class menu extends Phaser.Scene {
     this.cameras.main.setBackgroundColor('#111');
     this.load.setPath('assets/sprites');
     this.load.image('title', 'menus/titlecard.png');
+    this.load.image('startLevelScreen', 'menus/greyTile.png');
+    this.load.image('stageText', 'menus/stageText.png');
   }
   
   
@@ -15,6 +18,11 @@ class menu extends Phaser.Scene {
     //this.add.image(this.scale.width / 2, this.scale.height / 2, 'menuText');
     this.title = this.add.image(this.scale.width / 2, 300, 'title').setOrigin(0.5).setScale(0.5);
     this.startText = this.add.text(this.scale.width / 2, 380, 'Play', { fontSize: '16px', fill: '#ddd' }).setOrigin(0.5);
+
+    this.startLevelScreenUp = this.add.image(0, 0, 'startLevelScreen').setOrigin(0, 1).setScale(25);
+    this.startLevelScreenDown = this.add.image(0, this.scale.height, 'startLevelScreen').setOrigin(0).setScale(25);
+    this.levelText = this.add.image(GAME_SIZE.WIDTH / 2, GAME_SIZE.HEIGHT / 2, 'stageText').setOrigin(0.5).setAlpha(0);
+    
     //this.add.text(this.scale.width / 2 + 5, 180, '2 Players', { fontSize: '16px', fill: '#000' }).setOrigin(0.5);
     this.add.tween({
         targets: this.startText,
@@ -34,19 +42,32 @@ class menu extends Phaser.Scene {
   
     startGame() {
 
+        this.startText.destroy();
+        this.title.destroy();
         this.add.tween({
-            targets: this.startText,
+            targets: this.startLevelScreenUp,
             duration: 700,
-            alpha: 0
+            y : this.scale.height/2,
+            onComplete:this.showLevelText
         });
         this.add.tween({
-            targets: this.title,
-            duration: 1000,
-            alpha: 0,
-            onComplete:this.changeScene,
-            callbackScope: this
+            targets: this.startLevelScreenDown,
+            duration: 700,
+            y : this.scale.height/2
+            
         });
     }
+
+    showLevelText() {
+        console.log("showLevelText");
+        
+    }
+       /*this.add.tween({
+            targets: this.startLevelScreenDown,
+            duration: 10,
+            alpha: 1
+        });*/
+    
 
     changeScene() {
         this.scene.start("gameplayScene");
