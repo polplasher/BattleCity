6860f70ca396ba3b303cea6a0a5a68e003c1ebc5
loqatis <marc.serra@enti.cat>
2026-01-04 21:30:55 +0100

    Score screen apearence replication

--- src/scenes/ScoreMenu.js
+++ src/scenes/ScoreMenu.js
@@ -46,94 +46,105 @@ class ScoreMenu extends Phaser.Scene {
                     this.heavyDefeated += 1;
                     break;
                 case 500:
-                        // power-up;; TODO
+                        // power-up;;
                         break;
                 default:
                     break;
             }
         }
 
-
-        this.add.text(this.scale.width / 2, 10, 'Score Menu', { fontSize: '16px', fill: '#ddd' }).setOrigin(0.5);
+const centerX = this.scale.width / 2;
+    
         
         //Basic Tanks count
-        this.add.sprite(this.scale.width / 2 - 68, 120, 'numberSpritesheet', Math.floor(this.basicDefeated/10));
-        this.add.sprite(this.scale.width / 2 - 60, 120, 'numberSpritesheet', this.basicDefeated%10);
-        this.add.image(this.scale.width / 2 - 48, 119, '00Text');
-        this.add.sprite(this.scale.width / 2 - 28, 120, 'numberSpritesheet', Math.floor(this.basicDefeated/10));
-        this.add.sprite(this.scale.width / 2 - 20, 120, 'numberSpritesheet', this.basicDefeated%10);
-        this.add.image(this.scale.width / 2, 120, 'tank_basic');
+        this.add.sprite(centerX - 78, 120, 'numberSpritesheet', Math.floor(this.basicDefeated/10));
+        this.add.sprite(centerX - 70, 120, 'numberSpritesheet', this.basicDefeated%10);
+        this.add.image(centerX - 58, 119, '00Text');
+        this.add.image(centerX - 12, 121, 'arrow');
+        this.add.sprite(centerX - 30, 120, 'numberSpritesheet', Math.floor(this.basicDefeated/10));
+        this.add.sprite(centerX - 22, 120, 'numberSpritesheet', this.basicDefeated%10);
+        this.add.image(centerX, 120, 'tank_basic');
 
         //Fast Tanks count
-        this.add.sprite(this.scale.width / 2 - 68, 140, 'numberSpritesheet', Math.floor((this.fastDefeated*2)/10));
-        this.add.sprite(this.scale.width / 2 - 60, 140, 'numberSpritesheet', (this.fastDefeated*2)%10);
-        this.add.image(this.scale.width / 2 - 48, 139, '00Text');
-        this.add.sprite(this.scale.width / 2 - 28, 140, 'numberSpritesheet', Math.floor(this.fastDefeated/10));
-        this.add.sprite(this.scale.width / 2 - 20, 140, 'numberSpritesheet', this.fastDefeated%10);
-        this.add.image(this.scale.width / 2, 140, 'tank_fast');
-
+        this.add.sprite(centerX - 78, 140, 'numberSpritesheet', Math.floor((this.fastDefeated*2)/10));
+        this.add.sprite(centerX - 70, 140, 'numberSpritesheet', (this.fastDefeated*2)%10);
+        this.add.image(centerX - 58, 139, '00Text');
+        this.add.image(centerX - 12, 141, 'arrow');
+        this.add.sprite(centerX - 30, 140, 'numberSpritesheet', Math.floor(this.fastDefeated/10));
+        this.add.sprite(centerX - 22, 140, 'numberSpritesheet', this.fastDefeated%10);
+        this.add.image(centerX, 140, 'tank_fast');
         //Power Tanks count
-        this.add.sprite(this.scale.width / 2 - 68, 160, 'numberSpritesheet', Math.floor((this.powerDefeated*3)/10));
-        this.add.sprite(this.scale.width / 2 - 60, 160, 'numberSpritesheet', (this.powerDefeated*3)%10);
-        this.add.image(this.scale.width / 2 - 48, 159, '00Text');
-        this.add.sprite(this.scale.width / 2 - 28, 160, 'numberSpritesheet', Math.floor(this.powerDefeated/10));
-        this.add.sprite(this.scale.width / 2 - 20, 160, 'numberSpritesheet', this.powerDefeated%10);
-        this.add.image(this.scale.width / 2, 160, 'tank_power');
+        this.add.sprite(centerX - 78, 160, 'numberSpritesheet', Math.floor((this.powerDefeated*3)/10));
+        this.add.sprite(centerX - 70, 160, 'numberSpritesheet', (this.powerDefeated*3)%10);
+        this.add.image(centerX - 58, 159, '00Text');
+        this.add.image(centerX - 12, 161, 'arrow');
+        this.add.sprite(centerX - 30, 160, 'numberSpritesheet', Math.floor(this.powerDefeated/10));
+        this.add.sprite(centerX - 22, 160, 'numberSpritesheet', this.powerDefeated%10);
+        this.add.image(centerX, 160, 'tank_power');
 
         //Heavy Tanks count
-        this.add.sprite(this.scale.width / 2 - 68, 180, 'numberSpritesheet', Math.floor((this.heavyDefeated*4)/10));
-        this.add.sprite(this.scale.width / 2 - 60, 180, 'numberSpritesheet', (this.heavyDefeated*4)%10);
-        this.add.image(this.scale.width / 2 - 48, 179, '00Text');
-        this.add.sprite(this.scale.width / 2 - 28, 180, 'numberSpritesheet', Math.floor(this.heavyDefeated/10));
-        this.add.sprite(this.scale.width / 2 - 20, 180, 'numberSpritesheet', this.heavyDefeated%10);
-        this.add.image(this.scale.width / 2, 180, 'tank_armor');
+        this.add.sprite(centerX - 78, 180, 'numberSpritesheet', Math.floor((this.heavyDefeated*4)/10));
+        this.add.sprite(centerX - 70, 180, 'numberSpritesheet', (this.heavyDefeated*4)%10);
+        this.add.image(centerX - 58, 179, '00Text');
+        this.add.image(centerX - 12, 181, 'arrow');
+        this.add.sprite(centerX - 30, 180, 'numberSpritesheet', Math.floor(this.heavyDefeated/10));
+        this.add.sprite(centerX - 22, 180, 'numberSpritesheet', this.heavyDefeated%10);
+        this.add.image(centerX, 180, 'tank_armor');
     
         //Total Tanks count
         this.totalTankNumber = this.basicDefeated + this.fastDefeated + this.powerDefeated + this.heavyDefeated;
-        this.add.sprite(this.scale.width / 2 - 68, 200, 'numberSpritesheet', Math.floor((this.basicDefeated + this.fastDefeated * 2 + this.powerDefeated * 3 + this.heavyDefeated * 4)/10));
-        this.add.sprite(this.scale.width / 2 - 60, 200, 'numberSpritesheet', (this.basicDefeated + this.fastDefeated * 2 + this.powerDefeated * 3 + this.heavyDefeated * 4)%10);
-        this.add.image(this.scale.width / 2 - 48, 199, '00Text');
-        this.add.sprite(this.scale.width / 2 - 28, 200, 'numberSpritesheet', Math.floor(this.totalTankNumber/10));
-        this.add.sprite(this.scale.width / 2 - 20, 200, 'numberSpritesheet', this.totalTankNumber%10);
-        this.add.text(this.scale.width / 2 - 10, 200, 'TOTAL', { fontSize: '10px', fill: '#fff' }).setOrigin(0, 0.5);
+        this.add.sprite(centerX - 78, 200, 'numberSpritesheet', Math.floor((this.basicDefeated + this.fastDefeated * 2 + this.powerDefeated * 3 + this.heavyDefeated * 4)/10));
+        this.add.sprite(centerX - 70, 200, 'numberSpritesheet', (this.basicDefeated + this.fastDefeated * 2 + this.powerDefeated * 3 + this.heavyDefeated * 4)%10);
+        this.add.image(centerX - 58, 199, '00Text');
+        this.add.image(centerX - 12, 201, 'arrow');
+        this.add.sprite(centerX - 30, 200, 'numberSpritesheet', Math.floor(this.totalTankNumber/10));
+        this.add.sprite(centerX - 22, 200, 'numberSpritesheet', this.totalTankNumber%10);
+        this.add.text(centerX - 7, 200, 'TOTAL', { fontSize: '10px', fill: '#fff' }).setOrigin(0, 0.5);
 
-        const centerX = this.scale.width / 2;
+        
         const topScore = highScoreManager.getTopScore();
 
         // HI-SCORE en la parte superior
-        this.add.text(centerX, 20, 'HI-SCORE', {
+        
+        this.add.image(centerX - 50, 30, 'hiScoreText').setOrigin(0.5).setScale(1.5);
+        /*this.add.text(centerX, 20, 'HI-SCORE', {
             fontSize: '12px',
             fill: '#e44'
-        }).setOrigin(0.5);
+        }).setOrigin(0.5);*/
+        /*this.add.sprite(centerX + 50, 30, 'numberSpritesheet', Math.floor(this.topScore/1000)).setOrigin(0.5).setScale(1.5);
+        this.add.sprite(centerX + 58, 30, 'numberSpritesheet', this.topScore%10).setOrigin(0.5).setScale(1.5);
+        this.add.image(centerX + 58, 29, '00Text').setOrigin(0.5).setScale(1.5);*/
 
-        this.add.text(centerX, 38, topScore.toString().padStart(6, '0'), {
-            fontSize: '12px',
-            fill: '#e84'
-        }).setOrigin(0.5);
+        this.HIScoreThousends = this.add.sprite(centerX + 30, 30, 'numberSpritesheet', Math.floor(topScore/1000)).setOrigin(0.5).setScale(1.5);
+        this.HIScoreHundreds = this.add.sprite(this.HIScoreThousends.x + 12, 30, 'numberSpritesheet', Math.floor(topScore/100)%10).setOrigin(0.5).setScale(1.5);
+        this.HIScore0s = this.add.sprite(this.HIScoreHundreds.x + 20, 29, '00Text', 0).setOrigin(0.5).setScale(1.5);
 
+        
         // Stage
-        this.add.text(centerX, 70, `STAGE  ${this.stage}`, {
-            fontSize: '12px',
-            fill: '#fff'
-        }).setOrigin(0.5);
+        this.add.image(centerX - 30, 80, 'stageTextWhite').setOrigin(0.5).setScale(1.5);
+        this.add.sprite(centerX + 10, 80, 'numberSpritesheet', this.stage).setScale(1.5).setOrigin(0.5);
 
+        
         // Tu score
+        /*
         this.add.text(centerX - 60, 100, 'YOUR SCORE', {
             fontSize: '10px',
             fill: '#e84'
         }).setOrigin(0, 0.5);
-
+        */
+        /*
         this.add.text(centerX + 60, 100, this.playerScore.toString().padStart(6, '0'), {
             fontSize: '10px',
             fill: '#fff'
         }).setOrigin(1, 0.5);
+        */
 
         // Si es highscore nuevo
         if (this.isHighScore && this.playerScore > 0) {
             this.add.text(centerX, 220, '★ NEW HIGH SCORE! ★', {
                 fontSize: '10px',
                 fill: '#ff0'
-            }).setOrigin(0.5);
+            }).setOrigin(0.5).setScale(1.5);
 
             // Guardar automáticamente (en el futuro podrías pedir nombre)
             highScoreManager.addScore('PLAYER', this.playerScore, this.stage);
@@ -152,26 +163,26 @@ class ScoreMenu extends Phaser.Scene {
                 default:
                     reasonText = 'GAME OVER';
             }
-            this.add.text(centerX, 55, reasonText, {
+            this.add.text(centerX, 60, reasonText, {
                 fontSize: '10px',
                 fill: '#f44'
-            }).setOrigin(0.5);
+            }).setOrigin(0.5).setScale(1.5);
         }
 
         // Victory message
-        if (this.isVictory) {
+       /* if (this.isVictory) {
             if (this.isLastStage) {
-                this.add.text(centerX, 55, '★ CONGRATULATIONS! ★', {
+                this.add.text(centerX, 60, '★ CONGRATULATIONS! ★', {
                     fontSize: '10px',
                     fill: '#0f0'
-                }).setOrigin(0.5);
+                }).setOrigin(0.5).setScale(1.5);
             } else {
-                this.add.text(centerX, 55, 'STAGE CLEAR!', {
+                this.add.text(centerX, 60, 'STAGE CLEAR!', {
                     fontSize: '10px',
                     fill: '#0f0'
-                }).setOrigin(0.5);
+                }).setOrigin(0.5).setScale(1.5);
             }
-        }
+        }*/
 
         // Instrucción para continuar
         if (this.canContinue) {
